5ab972e9893ed07ec7b72619546db081888b2f47
k5md <serg.dfc@yandex.ru>
2019-02-12 22:14:40 +0300

    refactored 1/2

--- src/background.js
+++ src/background.js
@@ -2,91 +2,69 @@
 
 const loadFromStorage = (key = null) => browser.storage.local.get(key);
 
-
 class TabSuspender {
   constructor() {
-    this.filter = null;
     this.action = null;
-    this.tabs = [];
   }
 
-  // TODO: rework tabHandlers, removed, attach and detach, as well as diff windows NOT working
-  get tabHandlers() {
-    return {
-      onRemoved: (tabId) => {
-        bconsole.log(`tab ${tabId} removed`);
-        this.tabs = this.tabs.filter(cur => cur.id !== tabId);
-      },
-      onCreated: (tab) => {
-        bconsole.log(`tab ${tab.id} created`);
-        this.tabs = [...this.tabs, tab];
-        this.suspendTabs({ actionType: 'created', id: tab.id });
-      },
-      onActivated: ({ tabId }) => {
-        bconsole.log(`tab ${tabId} activated`);
-        // FIX: bug with multiple windows and thus multiple active tabs
-        const updatedTabs = this.tabs.map(tab => tab.id === tabId ? {...tab, active: true} : {...tab, active: false});
-        this.tabs = updatedTabs;
-        this.suspendTabs({ actionType: 'activated', id: tabId });
-      },
-      onAttached: (tabId) => {
-        bconsole.log(`tab ${tabId} attached`);
-      },
-      onDetached: (tabId) => {
-        bconsole.log(`tab ${tabId} detached`);
-      },
-    };
+  handleAction(actionInfo) {
+    browser.tabs.query({}).then((tabs) => {
+      this.action(tabs, actionInfo);
+    });
   }
 
   get config() {
+    // NOTE: actions are applied sequentially,
+    // modifiedTabs contain tabs changed in preceding actions, return them in actions!
     return [
       {
-        id: '#input-delay-suspend',
-        isEnabled: value => !Number.isNaN(parseInt(value, 10)) && parseInt(value, 10) >= 1,
-        action: (fn, value) => (tabsToDiscard, action) => {
-          const ms = parseInt(value, 10) * 1000;
-          // sets timeout for tabsToDiscard if no timeout has been set,
-          // removes the timeout if the tab has been activated
-          // TODO: add check for removed?
-          // TODO: somehow process loading tabs
-          const tabIdx = this.tabs.findIndex(tab => tab.id === action.id);
-
-          if (action.actionType === 'activated') {
-            bconsole.log('clearing timeout for', action.id);
-            clearTimeout(this.tabs[tabIdx].TabSuspenderTimeoutId);
-            this.tabs[tabIdx].TabSuspenderTimeoutId = null;
-          }
-
-          this.tabs.filter(tab => tab.id !== action.id && !tab.active).forEach((tab, tabIndex) => {
-            if (!tab.TabSuspenderTimeoutId) {
-              bconsole.log('setting timeout for', tab.id, tab.TabSuspenderTimeoutId);
-              const TabSuspenderTimeoutId = setTimeout(() => {
-                bconsole.log('time is out for tab', tab.id);
-                browser.tabs.discard(tab.id);
-                this.tabs[tabIndex].TabSuspenderTimeoutId = null;
-              }, ms);
-
-              this.tabs[tabIndex].TabSuspenderTimeoutId = TabSuspenderTimeoutId;
-            }
-          });
-          // NOTE: no fn call!
+        id: 'active',
+        action: () => (rawTabs, modifiedTabs = rawTabs, actionInfo) => {
+          console.log(rawTabs, modifiedTabs, actionInfo);
+          return modifiedTabs.filter(tab => !tab.active)
         },
-        defaultValue: '60', // value provided in seconds
+        isEnabled: () => true,
       },
       {
         id: '#input-ignore-audible',
+        action: () => (rawTabs, modifiedTabs = rawTabs) => modifiedTabs.filter(tab => !tab.audible),
         isEnabled: value => typeof value === 'boolean' && value,
-        filterFn: tabs => tabs.filter(tab => !tab.audible),
         defaultValue: false,
       },
       {
-        id: 'default',
-        action: fn => (tabsToDiscard, action) => {
-          bconsole.log('default action');
-          tabsToDiscard.map(tab => browser.tabs.discard(tab.id));
-          fn(tabsToDiscard, action);
+        id: '#input-delay-suspend',
+        action: value => (rawTabs, modifiedTabs = rawTabs) => {
+          bconsole.log('in delay', rawTabs, modifiedTabs);
+          const ms = parseInt(value, 10) * 1000;
+
+          const rest = rawTabs.filter(
+            thisTab => modifiedTabs.findIndex(tabToDiscard => tabToDiscard.id === thisTab.id) === -1,
+          );
+
+          // remove the timeout if the tab is not present in filter results
+          rest.forEach((tab) => {
+            bconsole.log('clearing timeout for', tab.id);
+            clearTimeout(this.delaySuspendTimeoutIds);
+            this.delaySuspendTimeoutIds[tab.id] = null;
+          });
+
+          filtered.forEach((tab) => {
+          // TODO: add check for removed?
+          // TODO: somehow process loading tabs
+            if (!this.delaySuspendTimeoutIds[tab.id]) {
+              bconsole.log('setting timeout for', tab.id);
+              const delaySuspendTimeoutId = setTimeout(() => {
+                bconsole.log('time is out for tab', tab.id);
+                browser.tabs.discard(tab.id);
+                this.delaySuspendTimeoutIds[tab.id] = null;
+              }, ms);
+
+              this.delaySuspendTimeoutIds[tab.id] = delaySuspendTimeoutId;
+            }
+          });
         },
-        isEnabled: () => true,
+        isEnabled: value => !Number.isNaN(parseInt(value, 10)) && parseInt(value, 10) >= 1,
+        defaultValue: '60', // value provided in seconds
       },
     ];
   }
@@ -100,23 +78,42 @@ class TabSuspender {
 
     const activeOptions = loadedOptions.filter(option => option.isEnabled(option.value));
 
-    // applies default, then filters from active options sequentially
-    const mergedFilters = activeOptions.reduce((acc, cur) => tabs => (typeof cur.filterFn === 'function' ? cur.filterFn(acc(tabs)) : acc(tabs)), tabs => tabs);
-    const mergedActions = activeOptions.reduceRight((acc, cur) => (typeof cur.action === 'function' ? cur.action(acc, cur.value) : acc), () => {});
+    const mergedActions = activeOptions.reduceRight(
+      (acc, cur) => (rawTabs, modTabs) => acc(rawTabs, cur.action(cur.value)(rawTabs, modTabs)),
+      (rawTabs, modTabs) => rawTabs,
+    );
 
-    this.filter = mergedFilters;
     this.action = mergedActions;
   }
 
-  suspendTabs(tabAction) {
-    browser.tabs.query({}).then((tabs) => {
-      bconsole.log(tabs, this.tabs, Object.is(this.tabs, tabs));
-      const tabsToDiscard = this.filter(tabs);
-      this.action(tabsToDiscard, tabAction);
-    });
+  get tabHandlers() {
+    return {
+      onCreated: (tab) => {
+        bconsole.log(`tab ${tab.id} created`);
+        this.handleAction({ type: 'created', id: tab.id });
+      },
+      onActivated: ({ tabId }) => {
+        bconsole.log(`tab ${tabId} activated`);
+        this.handleAction({ type: 'activated', id: tabId });
+      },
+      onUpdated: (tabId) => {
+        bconsole.log(`tab ${tabId} updated`);
+        this.handleAction({ type: 'updated', id: tabId });
+      },
+      onRemoved: (tabId) => {
+        bconsole.log(`tab ${tabId} removed`);
+      },
+      onAttached: (tabId) => {
+        bconsole.log(`tab ${tabId} attached`);
+      },
+      onDetached: (tabId) => {
+        bconsole.log(`tab ${tabId} detached`);
+      },
+    };
   }
 
   registerHandlers() {
+    // handle tab actions
     Object.keys(this.tabHandlers)
       .forEach(event => browser.tabs[event].addListener(this.tabHandlers[event]));
 
@@ -127,11 +124,9 @@ class TabSuspender {
   async run() {
     this.updateConfig = this.updateConfig.bind(this);
     this.registerHandlers = this.registerHandlers.bind(this);
-    this.suspendTabs = this.suspendTabs.bind(this);
+    this.handleAction = this.handleAction.bind(this);
     await this.updateConfig();
     this.registerHandlers();
-    const tabs = await browser.tabs.query({});
-    this.tabs = tabs;
   }
 }
 
